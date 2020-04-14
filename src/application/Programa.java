489f323687ca28ceaf4fb186cc2926cf58e2e4d2
diegosadu <diegosadu@gmail.com>
2020-04-14 13:30:46 -0300

    implementação do buscaPorId

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,22 +1,24 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.VendedorDao;
-import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Departamento obj = new Departamento(1, "Livros");
+		/*Departamento obj = new Departamento(1, "Livros");
 		System.out.println(obj);
 		
 		Vendedor vend = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		VendedorDao vendededorDao = DaoFactory.criaVendedorDao();
+		System.out.println(vend);*/
+		
+		VendedorDao vendedorDao = DaoFactory.criaVendedorDao();
+		Vendedor vend = vendedorDao.buscaPorId(3);
+		
 		System.out.println(vend);
 
 	}
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class VendedorDaoJDBC implements VendedorDao {
 
+	private Connection conn;
+	
+	public VendedorDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insere(Vendedor obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,47 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public Vendedor buscaPorId(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT v.*, d.nome DepNome "
+					+ "FROM departamento d, "
+					+ "vendedor v "
+					+ "WHERE v.id = ? "
+					+ "AND d.id = v.departamentoId");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			// se rs.next() retorna false significa que o executeQuery não gerou resultado
+			if(rs.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(rs.getInt("DepartamentoId"));
+				dep.setNome(rs.getString("DepNome"));
+				
+				Vendedor vend = new Vendedor();
+				vend.setId(rs.getInt("Id"));
+				vend.setNome(rs.getString("Nome"));
+				vend.setEmail(rs.getString("Email"));
+				vend.setDataNascimento(rs.getDate("DataNascimento"));
+				vend.setSalarioBase(rs.getDouble("SalarioBase"));
+				vend.setDepartamento(dep);
+				
+				return vend;
+			}
+			return null;	
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
 
 	@Override
