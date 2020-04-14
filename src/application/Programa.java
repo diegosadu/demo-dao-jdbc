f774128ec8257441e9dd8e0b4fcd4a2047f6839b
diegosadu <diegosadu@gmail.com>
2020-04-14 11:48:44 -0300

    Criação da classe Vendedor e ajustes na classe Departamento

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Programa {
 
@@ -8,6 +11,9 @@ public class Programa {
 		
 		Departamento obj = new Departamento(1, "Livros");
 		System.out.println(obj);
+		
+		Vendedor vend = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(vend);
 
 	}
 
